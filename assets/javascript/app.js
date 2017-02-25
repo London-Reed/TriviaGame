25ee9de6e5d45ec3e15132b51daad29c8e3e3235
London-Reed <london.reed1911@gmail.com>
2017-02-25 09:54:20 -0500

    fixed timer issue in html and jquery

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,10 +10,10 @@ var mins = 1;
 
     function Decrement() {
         currentMinutes = Math.floor(secs / 60);
-        currentSeconds = secs % 60;
+        currentSeconds = Math.floor(secs % 60);
         if(currentSeconds <= 9) currentSeconds = "0" + currentSeconds;
         secs--;
-        $("#display").append(currentMinutes + ":" + currentSeconds) ; 
+        $("#display").html("" + currentSeconds) ; 
         if(secs !== -1) setTimeout('Decrement()',1000);
     }
   

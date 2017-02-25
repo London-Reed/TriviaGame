ba17ddaaa128c9807cf75f0607d4d66637ec38ee
London-Reed <london.reed1911@gmail.com>
2017-02-25 07:23:28 -0500

    edited jquery file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,11 @@
-// STOPWATCH ACTIVITY (SOLUTION)
-// =============================
 
-// This code will run as soon as the page loads
 
-var mins = 1;  //Set the number of minutes you need
+
+var mins = 1;  
     var secs = mins * 60;
     var currentSeconds = 0;
     var currentMinutes = 0;
-    /* 
-     * The following line has been commented out due to a suggestion left in the comments. The line below it has not been tested. 
-     * setTimeout('Decrement()',1000);
-     */
+    
     setTimeout(Decrement,1000); 
 
     function Decrement() {
@@ -18,7 +13,7 @@ var mins = 1;  //Set the number of minutes you need
         currentSeconds = secs % 60;
         if(currentSeconds <= 9) currentSeconds = "0" + currentSeconds;
         secs--;
-        document.getElementById("display").innerHTML = currentMinutes + ":" + currentSeconds; //Set the element id you need the time put into.
+        $("#display").append(currentMinutes + ":" + currentSeconds) ; //Set the element id you need the time put into.
         if(secs !== -1) setTimeout('Decrement()',1000);
     }
   

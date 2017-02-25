0e518594e24ca35eb1535075e50573a2c31e5e13
London-Reed <london.reed1911@gmail.com>
2017-02-25 07:08:40 -0500

    added markup, started jquery

---
+++ assets/javascript/app.js
@@ -0,0 +1,24 @@
+// STOPWATCH ACTIVITY (SOLUTION)
+// =============================
+
+// This code will run as soon as the page loads
+
+var mins = 1;  //Set the number of minutes you need
+    var secs = mins * 60;
+    var currentSeconds = 0;
+    var currentMinutes = 0;
+    /* 
+     * The following line has been commented out due to a suggestion left in the comments. The line below it has not been tested. 
+     * setTimeout('Decrement()',1000);
+     */
+    setTimeout(Decrement,1000); 
+
+    function Decrement() {
+        currentMinutes = Math.floor(secs / 60);
+        currentSeconds = secs % 60;
+        if(currentSeconds <= 9) currentSeconds = "0" + currentSeconds;
+        secs--;
+        document.getElementById("display").innerHTML = currentMinutes + ":" + currentSeconds; //Set the element id you need the time put into.
+        if(secs !== -1) setTimeout('Decrement()',1000);
+    }
+  

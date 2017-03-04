0281c4cc30f9f24cc0ffb378fd159c8f0ae6f681
London-Reed <london.reed1911@gmail.com>
2017-03-04 08:33:34 -0500

    message

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,28 @@
 
 
 
-var mins = 1;  
+    var mins = 1;  
     var secs = mins * 60;
     var currentSeconds = 0;
     var currentMinutes = 0;
-    
+    var currentQuestion = 0;
+    var correctAnswers = 0;
+    var quizOver = false;
+
     setTimeout(Decrement,1000); 
+
+    var answers = ['Phil Jackson', 'Nate Robinson', 'Hakeem Olajuwon & Sam Bowie'],
+                    data = $("input"),
+                    correctAnswer = 0,
+                    incorrectAnswer = 0
+                    
+    
+    
+
+
+
+
+
 
     function Decrement() {
         currentMinutes = Math.floor(secs / 60);
@@ -15,5 +31,11 @@ var mins = 1;
         secs--;
         $("#display").html("" + currentSeconds) ; 
         if(secs !== -1) setTimeout('Decrement()',1000);
-    }
+    };
+
+
+    
+        
+    
+
   
